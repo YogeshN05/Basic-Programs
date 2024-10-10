8035f162df3e015944c870ae78a039c8947e94cc
Yogesh N <yyogesh532005@gmail.com>
2024-10-11 00:08:15 +0530

    Hello.java

--- Hello.java
+++ Hello.java
@@ -2,4 +2,9 @@ class Hello{
     public static void main(String []Yogi) {
         System.out.println("Hello Yogi");
     }
+    public static void new1()
+    {
+        System.out.println("Hi YOGI")
+    }
+        
 }

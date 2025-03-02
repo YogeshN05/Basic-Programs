0bcbbaeb422573c1f3ef71fd0889c7c4dd4e64ab
Yogi <yyogesh532005@gmail.com>
2025-03-02 19:20:16 +0530

    New Programs

---
+++ Java/Details.java
@@ -0,0 +1,14 @@
+//Write a program to print your name, age, and city
+
+public class Details {
+    public static void main(String[] args) {
+        String name="John";
+        int age=25;
+        String city="New York";
+
+        System.out.println("Name: "+name);
+        System.out.println("Age: "+age);
+        System.out.println("City: "+city);
+    }
+    
+}
--- Java/Hello.java
+++ Java/Hello.java
@@ -1,6 +1,7 @@
-class Hello{
+// Write a Java program to print "Hello, World!
+public class Hello{
     public static void main(String []Yogi) {
-        System.out.println("Hello Yogi");
+        System.out.println("Hello, World!");
     }
     public static void new1()
     {
